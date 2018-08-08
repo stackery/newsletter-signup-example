f80fb2d3f7bb9290cdeb46eb97540cc193b0d647
Sam Goldstein <sam@stackery.io>
2018-08-08 12:00:43 -0700

    Added code to return sign up form

--- src/getForm/index.js
+++ src/getForm/index.js
@@ -1,5 +1,26 @@
+const formHTML = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Newsletter Sign Up</title>
+  </head>
+  <body>
+  	<h1>Subscribe to our newsletter</h1>
+  	<form action="/submit" method="post">
+  		<label>Email
+  			<input name="email" type="text">
+  		</label>
+  		<input type="submit" value="Submit">
+  	</form>
+  
+  </body>
+</html>`;
 exports.handler = async message => {
   console.log(message);
 
-  return {};
-}
+  return {
+    statusCode: 200,
+    headers: {'Content-Type': 'text/html'},
+    body: formHTML
+  };
+};
