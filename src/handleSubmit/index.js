40e2a4221cfa509387ba405b9989cfc6cac4436b
Sam Goldstein <sam@stackery.io>
2018-08-16 16:41:02 -0700

    Implemented handleSubmit Lambda function

--- src/handleSubmit/index.js
+++ src/handleSubmit/index.js
@@ -1,5 +1,24 @@
+const qs = require('qs');
+const AWS = require('aws-sdk');
 exports.handler = async message => {
   console.log(message);
+  const formData = qs.parse(message.body);
+  console.log(formData);
 
-  return {};
-}
+  const dynamodb = new AWS.DynamoDB();
+  const params = {
+    Item: {
+      'email': {
+        S: formData.email
+      }
+    },
+    ReturnConsumedCapacity: 'TOTAL',
+    TableName: process.env.TABLE_NAME
+  };
+  await dynamodb.putItem(params).promise();
+
+  return {
+    statusCode: 302,
+    headers: {'Location': 'https://stackery.io'}
+  };
+};
