ef5ed6189eb8222c39f7b28aea00d9700fbd7b24
bildungsroman <anna.spysz@stackery.io>
2020-02-12 16:12:24 -0800

    Update functions

--- src/getForm/index.js
+++ src/getForm/index.js
@@ -5,22 +5,22 @@ const formHTML = `<!DOCTYPE html>
     <title>Newsletter Sign Up</title>
   </head>
   <body>
-  	<h1>Subscribe to our newsletter</h1>
-  	<form action="submit" method="post">
-  		<label>Email
-  			<input name="email" type="text">
-  		</label>
-  		<input type="submit" value="Submit">
-  	</form>
-  
+      <h1>Subscribe to our newsletter</h1>
+      <form action="submit" method="post">
+        <label>Email
+          <input name="email" type="text">
+        </label>
+        <input type="submit" value="Submit">
+      </form>
   </body>
 </html>`;
+
 exports.handler = async message => {
   console.log(message);
 
   return {
     statusCode: 200,
-    headers: {'Content-Type': 'text/html'},
+    headers: { 'Content-Type': 'text/html' },
     body: formHTML
   };
 };
